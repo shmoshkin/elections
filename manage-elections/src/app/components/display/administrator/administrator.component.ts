bbc8ab4c28df5f06ac748027a5b58edf0f985c6e
OrelKaravani <Orel1192@gmail.com>
2018-01-14 23:34:40 +0200

    adding buttons with basic  style

--- manage-elections/src/app/components/display/administrator/administrator.component.ts
+++ manage-elections/src/app/components/display/administrator/administrator.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material';
 @Component({
   selector: 'app-administrator',
   templateUrl: './administrator.component.html',
   styleUrls: ['./administrator.component.css']
 })
+
 export class AdministratorComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar:MatSnackBar) { }
 
   ngOnInit() {
   }
--- manage-elections/src/app/routing.module.ts
+++ manage-elections/src/app/routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisplayComponent } from './components/display/display.component';
+import {AdministratorComponent} from './components/display/administrator/administrator.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'selectors', pathMatch: 'full' },
-    { path: 'selectors', component: DisplayComponent }
+    { path: 'selectors', component: DisplayComponent },
+    { path: 'admin', component: AdministratorComponent }
   ];
 
 @NgModule({
